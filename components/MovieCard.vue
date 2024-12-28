89a47414ffca98a397a5ff460a42280965530f05
Danil Gabov <gabovdelovoy@gmail.com>
2024-12-29 00:12:16 +0500

    title one line max

--- components/MovieCard.vue
+++ components/MovieCard.vue
@@ -6,7 +6,7 @@
       class="relative z-10 bg-[#292D32] p-4 h-full transition-transform duration-300 group-hover:-translate-x-2 group-hover:-translate-y-2 rounded-lg shadow-lg active:outline outline-purple-600">
       <img :src="movie.poster" :alt="movie.name" class="w-full h-48 object-contain rounded-lg" />
       <div class="flex flex-col gap-2 mt-4">
-        <h2 class="text-lg xl:text-2xl font-bold">{{ movie.name }}</h2>
+        <h2 class="text-lg xl:text-2xl font-bold line-clamp-1">{{ movie.name }}</h2>
         <p class="text-sm overflow-hidden line-clamp-2">{{ movie.description }}</p>
         <div class="flex items-center space-x-2 mt-3">
           <MovieBadge :class="ratingBackground" icon="base-star-small">
